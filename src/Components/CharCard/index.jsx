b05034010eb429401320cd784733be0965fd800a
NadCarvalho <carvalho.nadiacaroline@gmail.com>
2021-09-16 17:51:10 -0300

    css finalizado

--- src/Components/CharCard/index.jsx
+++ src/Components/CharCard/index.jsx
@@ -1,10 +1,12 @@
-const CharCard = ({name, status, image}) => {
-    const isAlive = status === 'Alive' ? 'green': 'red'
-    return(
-<div className={isAlive}>
-    <h3>{name}</h3>
-    <img src={image} alt={name}></img>
-</div>
+import './index.css';
+ 
+const CharCard = ({ name, status, image }) => {
+    const isAlive = status === 'Alive' ? 'green' : 'red'
+    return (
+        <div className={`char ${isAlive}`}>
+            <h3>{name}</h3>
+            <img src={image} alt={name}></img>
+        </div>
     )
 }
 
--- src/Components/Characters/index.jsx
+++ src/Components/Characters/index.jsx
@@ -1,24 +1,25 @@
 import CharCard from "../CharCard"
+import './index.css'
+ 
+const Characters = ({ list }) => {
 
-const Characters = ({list}) => {
+  return (
+    <div className="my-characters">
+      <h3>Meus personagens</h3>
+      <ul>
+        {list.map(character =>
+          <li key={character.id}>
+            <CharCard
+              name={character.name}
+              status={character.status}
+              image={character.image} />
+          </li>
+        )}
 
-    return(
-<div>
-    <h3>Meus personagens</h3>
-    <ul className="my-characters">
-      {list.map(character =>
-        <li key={character.id}>
-          <CharCard 
-          name={character.name} 
-          status={character.status} 
-          image={character.image}/>
-        </li>
-      )}
+      </ul>
 
-    </ul>
-
-</div>
-    );
+    </div>
+  );
 }
 
 export default Characters;
